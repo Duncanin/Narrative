fccc7e9c9cf45edcdb985f10862328e87aac08a1
Duncan <ken211812@yahoo.com.tw>
2025-03-08 04:55:43 +0800

    fix gitignore

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,21 @@
-FROM openjdk:17-jdk-slim
+# 使用 Maven 3.8.4 和 OpenJDK 17 作為基礎映像
+FROM maven:3.8.4-openjdk-17 AS build
+
+# 複製 pom.xml 和源代碼
+COPY pom.xml .
+COPY src ./src
+
+# 執行 Maven 構建
+RUN mvn clean package -DskipTests
 
 # 設定工作目錄
 WORKDIR /Narrative
 
-# 複製 JAR 檔案
+# 使用 OpenJDK 17 作為運行時基礎映像
+FROM openjdk:17-jdk-slim
+
+# 從構建階段複製生成的 JAR 文件到運行時映像
 COPY target/narrative.jar /Narrative/narrative.jar
 
-# 啟動應用程式
-CMD ["java", "-jar", "narrative.jar"]
+# 設定容器啟動時執行的命令
+ENTRYPOINT ["java", "-jar", "/Narrative/narrative.jar"]
--- src/main/java/com/example/narrative/NarrativeApplication.java
+++ src/main/java/com/example/narrative/NarrativeApplication.java
@@ -1,10 +1,27 @@
 package com.example.narrative;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
 public class NarrativeApplication {
+
+	@Value("${Narrative}")
+	String name;
+
+	@RestController
+	class HelloController {
+		@GetMapping("/")
+		String hello() {
+			return "Hello , Welcome " + name + " ! " ;
+		}
+		
+		
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NarrativeApplication.class, args);
